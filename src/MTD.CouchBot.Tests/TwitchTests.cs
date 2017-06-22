88e9f67231766f931f8e5c4cbac4f0c681ac1f58
Matthew Smith <m.smithcpu@gmail.com>
2017-06-21 23:17:07 -0500

    Added Test Project

---
+++ src/MTD.CouchBot.Tests/TwitchTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace MTD.CouchBot.Tests
+{
+    public class TwitchTests
+    {
+        readonly string _twitchClientId = "TwitchClientId";
+
+        [Fact]
+        public void CanGetTwitchChannelLogOnId()
+        {
+
+        }
+    }
+}
